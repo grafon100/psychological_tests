7837c062ae822f93c4c0fa822ad7f72fcb689206
grafon100 <grafon100@hotmail.com>
2021-09-16 16:20:45 +0200

    add files

---
+++ psychological_tests/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  psychological_tests
+//
+//  Created by grafon100 on 16/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
